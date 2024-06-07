86e7219c61d15f851a760fb1c0d56bd6abc9dab5
Mert Özcan <iammertozcan@gmail.com>
2024-06-07 23:56:24 +0300

    Update Enumerations.Playground

--- 3.Types/Enumerations.playground/Contents.swift
+++ 3.Types/Enumerations.playground/Contents.swift
@@ -65,6 +65,11 @@ enum MilkyWayPlanet: Int {
     // We just hard-coded in the moon counts, which may be okay if you only have a small set of data but for larger data sets, we would want to do this all in a more dynamic fashion and let the computer do more of the work for us.
     
     // Methods
+    // - What is a method?
+    //      - A Function defined on Type
+    // - Usage:
+    //      - Declare that a method can change self's value with the mutating keyword.
+    
     func showMoonCount() {
         switch self {
         case .Mercury:
@@ -88,7 +93,7 @@ enum MilkyWayPlanet: Int {
     
     func showMoonNames() {
         switch self {
-        case MilkyWayPlanet(rawValue: 0)!:
+        case MilkyWayPlanet(rawValue: 0):
             print("No moons")
         case .Venus:
             print("No moons")
@@ -103,3 +108,58 @@ enum MilkyWayPlanet: Int {
         }
     }
 }
+
+MilkyWayPlanet.Earth.order      // Notice how properties and functions of enum Types are called using dot syntax.
+let firstPlanet: MilkyWayPlanet = .Mercury // Note that even thought the case Mercury exists on both Planet and MilkyWayPlanet, here it is still inferred to be of type MilkyWayPlanet because we wxplicitly typed our constant.
+
+// let thirdPlanet: .Earth // This returns an error because its type is ambiguous.
+
+let theMilkyWay: [MilkyWayPlanet] = [MilkyWayPlanet.Mercury, .Venus, .Earth, .Mars, .Jupiter, .Saturn, .Uranus, .Neptune]
+
+for planet in theMilkyWay {
+    planet.showMoonCount()
+    planet.showMoonNames()
+}
+
+let orderedSolarSystem = [0, 1, 2, 3, 4, 5, 6, 7]       // Notice that since MilkyWayPlanet is an Int value type, we can create instances using the (rawValue:) initializer to create instance of MilkyWayPlanet (below). But this is not very expressive.
+
+for planet in orderedSolarSystem {
+    MilkyWayPlanet(rawValue: planet)?.showMoonCount()
+}
+
+// Limitations
+// - Note  that an enumeration can contain calculated properties but not stored properties
+// - Models are more flexible and concise when they can contain stored properties (e.g. Structures and Classes)
+// A Class or Structure would be a much better Type to use for the planets of different solar systems.
+// It would be much more powerful to use an enum for values of a property of a planet. For example...
+
+class CelestialBody: NSObject {
+    enum CelestialBodyTaxon: Int {
+        case Unknown = 0
+        case Star
+        case Planet
+        case Moon
+        case DwarfPlanet
+    }
+    
+    var taxonomy: CelestialBodyTaxon!
+    
+    convenience init(taxonomy: Int) {
+        self.init()
+        
+        switch taxonomy {
+        case 1:
+            // Notice we use the (rawValue:) initializer so we can read in Int values (such as might come from a database) and not worry about mapping which is which - the enum does that for us.
+            self.taxonomy = CelestialBodyTaxon.init(rawValue: 0)
+        case 2:
+            self.taxonomy = CelestialBodyTaxon.init(rawValue: 1)
+        case 3:
+            self.taxonomy = CelestialBodyTaxon.init(rawValue: 2)
+        default:
+            self.taxonomy = .Unknown
+        }
+    }
+}
+
+let planet = CelestialBody(taxonomy: 2)
+print(planet.taxonomy!)
